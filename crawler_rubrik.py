9bd079b56615d4913f67b9cb4c4adb8ae6dc02c9
taronhov <36854313+taronhov@users.noreply.github.com>
2018-07-11 19:08:19 +0400

    v1.1: some minor updtaes in examples (accessing GitHub through its JSON API).

--- crawler_rubrik.py
+++ crawler_rubrik.py
@@ -5,7 +5,8 @@ from rethinkdb.errors import RqlRuntimeError, RqlDriverError, ReqlOpFailedError
 import json
 
 """  This is a data crawler/scraper script foo Rubrik.com website, using its JSON API capabilities and 
-    and utilizing Python driver and ReQL query language of Rethink DB (r.http command for accessing external APIs)"""
+    and utilizing Python driver and ReQL query language of Rethink DB (r.http command for accessing external APIs)
+"""
 
 def dbSetup():
 
@@ -21,25 +22,36 @@ def dbSetup():
         connection.close()
 
 def main():
+    # Setting-up the database:
     dbSetup()
+    # Connecting to web-sites to reseive data:
     connection = r.connect( "localhost", 28015)
     try:
         r.table("stargazers").insert(r.http('https://api.github.com/repos/rethinkdb/rethinkdb/stargazers')).run(connection)
-        
+
+        stg_count = r.http('https://api.github.com/repos/rethinkdb/rethinkdb/stargazers').count().run(connection)
+
+        name_id = r.http('https://api.github.com/repos/rethinkdb/rethinkdb/stargazers').pluck('login', 'id').order_by('id').run(connection)
+
+
         # r.table("rubrik").insert(r.http('https://www.rubrik.com/customers/', result_format='json')).run(connection)
+
         # r.table("google").insert(r.http('www.google.com')).run(connection)
 
         # cursor = r.table("stargazers").run(connection)
         # for document in cursor:
         #     print(document)
-        aaa = r.http('https://api.github.com/repos/rethinkdb/rethinkdb/stargazers').count().run(connection)
-        print (aaa)
-        cursor = r.table("google").run(connection)
-        for document in cursor:
-            print(document)
+        
+        print (stg_count)
+
+        print (name_id)
+
+        # cursor = r.table("google").run(connection)
+        # for document in cursor:
+        #     print(document)
 
     except ReqlOpFailedError:
-        print 'Table already exists in database.'
+        print 'Table already exists in database (while reading) or is inaccessible (while writing).'
     finally:
         connection.close()
 
